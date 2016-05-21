4f43ac63a0f62a03c1e7e035aded5b050b576e4c
drielnox <bl4z3r@gmail.com>
2016-05-21 20:14:44 -0300

    Se agregaron las primeras entidades de negocio a la solucion.

---
+++ src/Server/Application/Business Entities/Almacen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business_Entities
+{
+    public class Almacen
+    {
+        public IEnumerable<Zona> Zonas { get; set; }
+
+        public Almacen()
+        {
+            Zonas = new List<Zona>();
+        }
+    }
+}
---
+++ src/Server/Application/Business Entities/DetectorDeMovimiento.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business_Entities
+{
+    public abstract class DetectorDeMovimiento : Sensor
+    {
+    }
+}
